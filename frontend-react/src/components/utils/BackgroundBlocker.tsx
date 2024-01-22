e6a53e96b6c79324863ecd786e5cf29a1daf6420
Epictigu <tigusa.r@gmail.com>
2024-01-22 19:01:06 +0100

    Added BackgroundBlocker

--- frontend-react/src/components/utils/BackgroundBlocker.tsx
+++ frontend-react/src/components/utils/BackgroundBlocker.tsx
@@ -1,3 +1,5 @@
+import {CSSProperties} from "react";
+
 const defaultProps = {
     backgroundColor: "transparent",
     customZIndex: 50
@@ -5,5 +7,14 @@ const defaultProps = {
 type BackgroundProps = Partial<typeof defaultProps>;
 
 function BackgroundBlocker(props: BackgroundProps) {
+    const style: CSSProperties = {
+        backgroundColor: props.backgroundColor,
+        zIndex: props.customZIndex
+    }
 
+    return (<>
+        <div className="click-blocker" style={style}></div>
+    </>)
 }
+
+export default BackgroundBlocker;
